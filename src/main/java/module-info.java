232f0b4103afc94832be2850ee24bdd58a06ace5
Peter Keappock <peterkeappock@Peters-MacBook-Air.local>
2024-11-11 15:47:14 +0000

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.assignment120068658 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.assignment120068658 to javafx.fxml;
+    exports com.example.assignment120068658;
+}
